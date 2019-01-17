64e88f2e9908539d84218991c1f3dc71c7f836cb
bradfox2 <bradfox2@gmail.com>
2019-01-16 22:01:57 -0700

    Simple running request workflow.

--- nrc_adams_py/core.py
+++ nrc_adams_py/core.py
@@ -28,7 +28,9 @@ class Payload(object):
 ## q, tab, qn, s, so
 
 
-q = '''(mode:sections,sections:( filters:(public-library:!t),),properties_search_any:!(!(DocumentType,ends,'Enforcement+Action','')),single_content_search:'Gamma+Knife’))'''
+q = '''(mode:sections,sections:(filters:(public-library:!t),properties_search_any:!(!(DocumentType,ends,'Enforcement+Action','')),single_content_search:'Gamma+Knife'))'''
+
+#print(q)
 
 tab = 'content-search-pars'
 
@@ -42,22 +44,11 @@ p = Payload(q, tab, qn, s, so)
 
 load = p.generate_payload()
 
-#working
-#https://adams.nrc.gov/wba/services/search/advanced/nrc?q=(mode:sections,sections:(filters:(public-library:!t),properties_search_any:!(!(DocumentType,ends,%27Enforcement+Action%27,%27%27)),single_content_search:%27Gamma+Knife%27))&qn=New&tab=content-search-pars&s=PublishDatePARS&so=ASC
-
-#not
-#https://adams.nrc.gov/wba/services/search/advanced/nrc?q=%28mode%3Asections%2Csections%3A%28+filters%3A%28public-library%3A%21t%29%2C%29%2Cproperties_search_any%3A%21%28%21%28DocumentType%2Cends%2C%27Enforcement%2BAction%27%2C%27%27%29%29%2Csingle_content_search%3A%27Gamma%2BKnife%C3%A2%E2%82%AC%E2%84%A2%29%29&tab=content-search-pars&qn=New&s=PublishDatePARS&so=ASC
-
-#http://adams.nrc.gov/wba/services/search/advanced/nrc?q=( mode:sections,sections:( filters:( public-library:!t), ),properties_search_any:!( !(DocumentType,ends,'Enforcement+Action','') ),single_content_search:'Gamma+Knife’ ) )&qn=New&tab=content-search-pars&s=DocumentDate&so=DESC
-
-
-#requests is stripping parens
-r  = requests.get(NRC_BASE_URL, params = load)
-
-url = r.url
+r = requests.get(NRC_BASE_URL, params = load)
 
 print(r.url)
-
 print(r.content)
 
 tree = ElementTree.fromstring(r.content)
+
+ElementTree.dump(tree)
